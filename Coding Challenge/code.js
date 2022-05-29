33a94a8fdd2203df664b1f1812d9aef37d155df7
Anwesh Mishra <anweshm3@gmail.com>
2022-05-29 22:42:12 +0530

    Coding Challenge #3

--- Coding Challenge/code.js
+++ Coding Challenge/code.js
@@ -1,4 +1,4 @@
-// Coding Challenge #1
+// Coding Challenge #1 && #2
 
 // Marks
 const marksWt1 = 78
@@ -21,4 +21,34 @@ if (marksBMI1 > johnBMI1) {
    console.log(`Mark's BMI (${marksBMI1}) is higher than John's (${johnBMI1})!`)
 } else {
    console.log(`John's BMI (${johnBMI1}) is higher than Mark's (${marksBMI1})!`)
+}
+
+
+
+// Coding Challenge #3
+/*
+const scoreDolphins = (96 + 108 + 89) / 3
+const scoreKoalas = (88 + 91 + 110) / 3
+
+if (scoreDolphins === scoreKoalas) {
+   console.log('Both wins the Trophy 🏆')
+} else if (scoreDolphins > scoreKoalas) {
+   console.log('Dolphins win the Trophy 🏆')
+} else {
+   console.log('Koalas win the Trophy 🏆')
+}
+*/
+
+const scoreDolphins = (97 + 112 + 101) / 3
+const scoreKoalas = (109 + 95 + 123) / 3
+console.log(scoreDolphins, scoreKoalas)
+
+if ((scoreDolphins === scoreKoalas) || (scoreDolphins >= 100 && scoreKoalas >= 100)) {
+   console.log('Both wins the Trophy 🏆')
+} else if ((scoreDolphins > scoreKoalas) && (scoreDolphins >= 100)){
+   console.log('Dolphins win the Trophy 🏆')
+} else if ((scoreKoalas > scoreDolphins) && (scoreKoalas >= 100)) {
+   console.log('Koalas win the Trophy 🏆')
+} else {
+   console.log('No team wins the Trophy 🏆')
 }
