7014ac5510246317e8e98bebf6bc4ffb2cddd4d8
Anwesh Mishra <anweshm3@gmail.com>
2022-05-29 23:20:02 +0530

    Switch Statement

--- JavaScript Fundamentals - Part 1/day1.js
+++ JavaScript Fundamentals - Part 1/day1.js
@@ -263,3 +263,46 @@ if (hasDriverLicense && hasGoodVision && !isTired) {
 } else {
    console.log('Someone else should drive...')
 }
+
+////////////////////////////////////
+// Switch Statement
+const day = 'monday'
+
+switch (day) {
+   case 'monday': // day === 'monday'
+      console.log('Plan course Structure')
+      console.log('Go to coding meetup')
+      break
+   case 'tuesday':
+      console.log('Prepare theory videos')
+      break
+   case 'wednesday':
+   case 'thursday':
+      console.log('Write code examples')
+      break
+   case 'friday':
+      console.log('Record Videos')
+      break
+   case 'saturday':
+   case 'sunday':
+      console.log('Enjoy the weekend :D')
+      break
+   default:
+      console.log('Not a valid day!')
+      break
+}
+
+if (day === 'monday') {
+   console.log('Plan course Structure')
+   console.log('Go to coding meetup')
+} else if (day === 'tuesday') {
+   console.log('Prepare theory videos')
+} else if ((day === 'wednesday') || (day === 'thursday')) {
+   console.log('Write code examples')
+} else if (day === 'friday') {
+   console.log('Record Videos')
+} else if ((day === 'saturday') || (day === 'sunday')) {
+   console.log('Enjoy the weekend :D')
+} else {
+   console.log('Not a valid day!')
+}
