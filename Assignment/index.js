6e77a4baf8e573714890543fdc638e413de5202f
Anwesh Mishra <anweshm3@gmail.com>
2022-05-29 14:36:09 +0530

    Assignment

---
+++ Assignment/index.js
@@ -0,0 +1,29 @@
+const country = "India"
+const continent = "Asia"
+let population = 130000000
+
+console.log(country, continent, population)
+
+const isIsland = false
+let language
+
+console.log(typeof isIsland)
+console.log(typeof country)
+console.log(typeof population)
+console.log(typeof language)
+
+language = 'Hindi'
+// isIsland = true
+
+console.log(population / 2)
+population += 1;
+console.log(population)
+let finPopulation = 6000000
+console.log(population > finPopulation)
+avg = 33000000
+console.log(population < avg)
+const description1 = 'Portugal is in Europe, and its 11 million people speak portuguese'
+console.log(description1)
+
+const description = `${country} is in ${continent} and its 1.3 billion people speak ${language}`
+console.log(description)
