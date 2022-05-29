8977d1671a43f2464f5f058cf38eee049e3d0259
Anwesh Mishra <anweshm3@gmail.com>
2022-05-29 22:11:54 +0530

    Equality Operators

--- JavaScript Fundamentals - Part 1/day1.js
+++ JavaScript Fundamentals - Part 1/day1.js
@@ -203,3 +203,31 @@ if (height) {
 } else {
    console.log('Height is UNDEFINED')
 }
+
+////////////////////////////////////
+// Equality Operators
+const personAge = '18'
+
+// === Strict Equality Operator
+if (personAge === 18) console.log('You just became an adult :D (strict)') 
+
+// == Loose Equality Operator
+if (personAge == 18) console.log('You just became an adult :D (loose)')
+
+// const favorite =  Number(prompt("What's your favorite number ?"))
+console.log(favorite)
+console.log(typeof favorite)
+
+if (favorite === 23) { // 22 === 23 -> FALSE
+   console.log('Cool! 23 is an amazing number!')
+} else if (favorite === 7) {
+   console.log('7 is also a cool number')
+} else if (favorite === 9) {
+   console.log('9 is also a cool number')
+} else {
+   console.log('Number is not 23 or 7')
+}
+
+if (favorite !== 23 ) {
+   console.log('Why not 23?')
+}
