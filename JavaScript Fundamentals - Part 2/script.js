cb8d0570998ea06e0987941dce3e7dec280b6460
Anwesh Mishra <anweshm3@gmail.com>
2022-06-01 16:16:37 +0530

    Iteration: The For Loop

--- JavaScript Fundamentals - Part 2/script.js
+++ JavaScript Fundamentals - Part 2/script.js
@@ -248,3 +248,21 @@ console.log(anwesh.age)
 console.log(anwesh.getSummary())
 
 // console.log(anwesh['calculateAge'](1991))
+
+///////////////////////
+// Iteration: The For Loop
+// console.log('Lifting weights repetition 1 🏋️')
+// console.log('Lifting weights repetition 2 🏋️')
+// console.log('Lifting weights repetition 3 🏋️')
+// console.log('Lifting weights repetition 4 🏋️')
+// console.log('Lifting weights repetition 5 🏋️')
+// console.log('Lifting weights repetition 6 🏋️')
+// console.log('Lifting weights repetition 7 🏋️')
+// console.log('Lifting weights repetition 8 🏋️')
+// console.log('Lifting weights repetition 9 🏋️')
+// console.log('Lifting weights repetition 10 🏋️')
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+   console.log(`Lifting weights repetition ${rep} 🏋️`)
+}
