98e1a194931d3b58b1379eacf10183848b33ae61
Anwesh Mishra <anweshm3@gmail.com>
2022-06-01 11:22:35 +0530

    Coding Challenge #6

--- Coding Challenge/code.js
+++ Coding Challenge/code.js
@@ -68,6 +68,7 @@ console.log(`This was ${bill}, the tip was ${tip} and the total value ${bill + t
 // Coding Challenge #1
 const calcAvg = (score1, score2, score3) => (score1 + score2 + score3) / 3
 
+// Test 1
 let avgDolphins = calcAvg(44, 23, 71)
 let avgKoalas = calcAvg(65, 54, 49)
 
@@ -82,3 +83,17 @@ const checkWinners = function (avgDolphins, avgKoalas) {
 }
 checkWinners(avgDolphins, avgKoalas)
 checkWinners(576, 111)
+
+// Test 2
+avgDolphins = calcAvg(85, 54, 41)
+avgKoalas = calcAvg(23, 34, 27)
+checkWinners(avgDolphins, avgKoalas)
+
+
+
+// Coding Challenge #2
+const calcTip = (bil) => (bil <= 300 && bil >= 50) ? (bil * 0.15) : (bil * 0.20)
+const bills = [125, 555, 44]
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+const totals = [(bills[0] + tips[0]), (bills[1] + tips[1]), (bills[2] + tips[2])]
+console.log(bills, tips, totals)
