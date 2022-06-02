3dc11abe2b4b9deb6e86a24a2e1fbf1300a69572
Anwesh Mishra <anweshm3@gmail.com>
2022-06-02 23:04:17 +0530

    First Commit

---
+++ JavaScript DOM and Events Fundamentals/Project 2 - Modal Window/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (e) => {
+   // console.log('A key was pressed')
+   console.log(e.key)
+   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal()
+   }
+})
