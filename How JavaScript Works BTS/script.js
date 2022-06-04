9c6c9ebce5ac544a93344ce17b9152b1707a2a78
Anwesh Mishra <anweshm3@gmail.com>
2022-06-04 10:15:06 +0530

    Hoisting and TDZ in Practice

--- How JavaScript Works BTS/script.js
+++ How JavaScript Works BTS/script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+///////////////////////////////////////
+// Scoping in Practice
 function calcAge(birthYear) {
    const age = 2037 - birthYear
 
@@ -35,3 +37,48 @@ const firstName = 'Anwesh'
 calcAge(1991)
 // console.log(age)
 // printAge()
+
+///////////////////////////////////////
+// Hoisting and TDZ in Practice
+// Variables
+console.log(me)
+// console.log(job)
+// console.log(year)
+
+var me = 'Anwesh'
+let job = 'Teacher'
+const year = 1991
+
+// Functions
+console.log(addDecl(2, 3))
+// console.log(addExpr(2, 3))
+console.log(addArrow)
+// console.log(addArrow(2, 3))
+
+function addDecl(a, b) {
+   return a + b
+}
+
+const addExpr = function (a, b) {
+   return a + b
+}
+
+var addArrow = (a, b) => a + b
+
+// Example
+console.log(undefined)
+if(!numProducts) deleteShoppingCart()
+
+var numProducts = 10
+
+function deleteShoppingCart() {
+   console.log('All products deleted')
+}
+
+var x = 1
+let y = 2
+const z = 3
+
+console.log(x === window.x)
+console.log(y === window.y)
+console.log(z === window.z)
