03082cd01beefb4bcc2c64009de52b5e1602d217
Anwesh Mishra <anweshm3@gmail.com>
2022-06-05 16:26:48 +0530

    The Nullish Coalescing Operator

--- Data Structures, Modern Operators and Strings/script.js
+++ Data Structures, Modern Operators and Strings/script.js
@@ -46,6 +46,16 @@ const restaurant = {
   },
 };
 
+///////////////////////////////////////
+// The Nullish Coalescing Operator
+restaurant.numGuests = 0
+const guests = restaurant.numGuests || 10
+console.log(guests)
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10
+console.log(guestCorrect)
+
 /*
 ///////////////////////////////////////
 // Short Circuiting (&& and ||)
