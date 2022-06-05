d3c312a8ce1574e8ca015d8469eb6e3f41ed5363
Anwesh Mishra <anweshm3@gmail.com>
2022-06-05 16:46:13 +0530

    Coding Challenge #8

--- Coding Challenge/code.js
+++ Coding Challenge/code.js
@@ -132,7 +132,7 @@ if (mark.calcBMI() < john.calcBMI) {
 
 
 
-// Coding Challenge #3
+// Coding Challenge #4
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
 const tips = []
 const totals = []
@@ -157,3 +157,80 @@ console.log(calcAverage([2, 3, 7]))
 console.log(calcAverage(totals))
 console.log(calcAverage(tips))
 */
+
+
+
+// Data Structures, Modern Operators and Strings
+// Coding Challenge #1
+const game = {
+   team1: 'Bayern Munich',
+   team2: 'Borrussia Dortmund',
+   players: [
+     [
+       'Neuer',
+       'Pavard',
+       'Martinez',
+       'Alaba',
+       'Davies',
+       'Kimmich',
+       'Goretzka',
+       'Coman',
+       'Muller',
+       'Gnarby',
+       'Lewandowski',
+     ],
+     [
+       'Burki',
+       'Schulz',
+       'Hummels',
+       'Akanji',
+       'Hakimi',
+       'Weigl',
+       'Witsel',
+       'Hazard',
+       'Brandt',
+       'Sancho',
+       'Gotze',
+     ],
+   ],
+   score: '4:0',
+   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+   date: 'Nov 9th, 2037',
+   odds: {
+     team1: 1.33,
+     x: 3.25,
+     team2: 6.5,
+   },
+ };
+
+//  1.
+ const [players1, players2] = game.players
+ console.log(players1, players2)
+
+//  2.
+ const [gk, ...fieldPlayers] = players1
+ console.log(gk, fieldPlayers)
+
+//  3.
+ const allPlayers = [...players1, ...players2]
+ console.log(allPlayers)
+
+//  4.
+ const finalPlayers = [...players1, 'Thiago', 'Coutinho', 'Peristic']
+
+//  5.
+ const {odds: {team1, x: draw, team2}} = game
+ console.log(team1, draw, team2)
+
+//  6.
+ const printGoals = function (...Players) {
+    console.log(Players)
+    console.log(`${Players.length} goals were scored`)
+ }
+ printGoals('Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels')
+ printGoals('Lewandowski', 'Hummels')
+ printGoals(...game.scored)
+
+//  7.
+team1 < team2 && console.log(`Team 1 is more likely to win`)
+team1 > team2 && console.log(`Team 2 is more likely to win`)
