1fd1e549a94a942d0bf03ba2d2341b424286ab60
Anwesh Mishra <anweshm3@gmail.com>
2022-06-05 23:05:55 +0530

    Enhanced Object Literals

--- Data Structures, Modern Operators and Strings/script.js
+++ Data Structures, Modern Operators and Strings/script.js
@@ -1,31 +1,36 @@
 'use strict';
 
-///////////////////////////////////////
-// Destructuring Arrays
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+
+const openingHours = {
+   [weekdays[3]]: {
+     open: 12,
+     close: 22,
+   },
+   [weekdays[4]]: {
+     open: 11,
+     close: 23,
+   },
+   [weekdays[5]]: {
+     open: 0,
+     close: 24,
+   },
+ }
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0,
-      close: 24,
-    },
-  },
-  order: function (starterIndex, mainIndex) {
+
+ // ES6 enhanced object literals
+  hours,
+
+  order (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-  orderDelivery: function ({
+  orderDelivery ({
     starterIndex = 1,
     mainIndex = 0,
     time = '20:00',
@@ -35,17 +40,18 @@ const restaurant = {
       `Order Received ! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta (ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza (mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 };
 
+/*
 ///////////////////////////////////////
 // The for-of Loop
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu]
@@ -58,7 +64,6 @@ for (const [i, el] of menu.entries()) {
 
 // console.log([...menu.entries()])
 
-/*
 ///////////////////////////////////////
 // The Nullish Coalescing Operator
 restaurant.numGuests = 0
