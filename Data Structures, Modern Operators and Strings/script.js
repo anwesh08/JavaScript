e51feff11c5659c1ea58d698807453016cbe41f4
Anwesh Mishra <anweshm3@gmail.com>
2022-06-06 11:34:48 +0530

    Maps: Fundamentals

--- Data Structures, Modern Operators and Strings/script.js
+++ Data Structures, Modern Operators and Strings/script.js
@@ -47,6 +47,39 @@ const restaurant = {
 };
 
 ///////////////////////////////////////
+// Maps: Fundamentals
+const rest = new Map()
+rest.set('name', 'Classico Italiano')
+rest.set(1, 'Firenze, Italy')
+rest.set(2, 'Lisbon, Portugal')
+console.log(rest)
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11).set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(')
+
+console.log(rest.get('name'))
+console.log(rest.get(true))
+console.log(rest.get(1))
+
+const time = 21
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')))
+
+console.log(rest.has('categories'))
+rest.delete(2)
+// rest.clear()
+const arr = [1, 2]
+rest.set(arr, 'Test')
+rest.set(document.querySelector('h1'), 'Heading')
+console.log(rest)
+console.log(rest.size)
+
+console.log(rest.get(arr))
+
+/*
+///////////////////////////////////////
 // Sets
 const ordersSet = new Set([
   'pasta',
@@ -84,7 +117,6 @@ console.log(
 
 console.log(new Set('AnweshMishra').size)
 
-/*
 ///////////////////////////////////////
 // Looping Objects: Object Keys, Values, and Entries
 
