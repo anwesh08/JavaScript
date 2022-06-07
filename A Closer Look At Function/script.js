3943a0c0f0e5a2e1cc4ffd3c6c8343a95fd02c19
Anwesh Mishra <anweshm3@gmail.com>
2022-06-07 10:26:25 +0530

    How Passing Arguments Works: Values vs. Reference

--- A Closer Look At Function/script.js
+++ A Closer Look At Function/script.js
@@ -26,3 +26,36 @@ createBooking('LH123', 2)
 createBooking('LH123', 5)
 
 createBooking('LH123', undefined, 1000)
+
+///////////////////////////////////////
+// How Passing Arguments Works: Values vs. Reference
+const flight = 'LH234'
+const anwesh = {
+   name: 'Anwesh Mishra',
+   passport: 25479302720
+}
+
+const checkIn = function (flightNum, passenger) {
+   flightNum = 'LH99'
+   passenger.name = 'Mr.' + passenger.name
+   if (passenger.passport === 25479302720) {
+      alert('Checked In')
+   } else {
+      alert('Wrong Passport!')
+   }
+}
+
+// checkIn(flight, anwesh)
+// console.log(flight)
+// console.log(anwesh)
+
+// Is the same as doing...
+// const flightNum = flight
+// const passenger = anwesh
+
+const newPassport = function(person) {
+   person.passport = Math.trunc(Math.random() * 1000000000)
+}
+
+newPassport(anwesh)
+checkIn(flight, anwesh)
