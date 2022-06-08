2b731667b1f10f4cfd08bc096de476ab9082c7b6
Anwesh Mishra <anweshm3@gmail.com>
2022-06-08 18:47:24 +0530

    Coding Challenge #15

--- Coding Challenge/code.js
+++ Coding Challenge/code.js
@@ -361,24 +361,34 @@ poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]})
 })();
 */
 
-
-
 // Working with Arrays
 // Coding Challenge #1
 const checkDogs = (dogsJulia, dogsKate) => {
-   const dogsJuliaCorrected = dogsJulia.slice()
-   dogsJuliaCorrected.splice(0, 1)
-   dogsJuliaCorrected.splice(-2)
-   const dogs = dogsJuliaCorrected.concat(dogsKate)
-   dogs.forEach( (dog, i) => {
-      if (dog >= 3) {
-         console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old.`)
-      } else {
-         console.log(`Dog number ${i + 1} is still a puppy 🐶`)
-      }
-   });
-}
-console.log('----TEST DATA 1----')
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
-console.log('----TEST DATA 2----')
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4])
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  dogs.forEach((dog, i) => {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old.`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+    }
+  });
+};
+console.log("----TEST DATA 1----");
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+console.log("----TEST DATA 2----");
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+// Coding Challenge #2
+const calcAverageHumanAge = (ages) => {
+  const humanAge = ages.map(age => (age <= 2 ? 2 * age : 16 + 4 * age));
+  const adults = humanAge.filter(age => age >= 18);
+  console.log(humanAge);
+  console.log(adults);
+  return adults.reduce((acc, age) => acc + age, 0) / adults.length;
+};
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
