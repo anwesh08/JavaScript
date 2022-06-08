7bd0bc31f19426176651c12aae751b37f507e91f
Anwesh Mishra <anweshm3@gmail.com>
2022-06-08 18:05:37 +0530

    The filter Method

--- Working with Arrays/script.js
+++ Working with Arrays/script.js
@@ -117,3 +117,22 @@ const movementsDescription = movements.map( (mov, i) =>
 )
 
 console.log(movementsDescription)
+
+///////////////////////////////////////
+// The filter Method
+const deposits = movements.filter( mov => {
+  return mov > 0
+})
+console.log(movements)
+console.log(deposits)
+
+const depositsFor = []
+for (const mov of movements) {
+  if (mov > 0) {
+    depositsFor.push(mov)
+  }
+}
+console.log(depositsFor)
+
+const withdrawals = movements.filter( mov => mov < 0)
+console.log(withdrawals)
