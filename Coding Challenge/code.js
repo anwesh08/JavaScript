f7db2e0e6c1665d12fba0867c19c744729a00c04
Anwesh Mishra <anweshm3@gmail.com>
2022-06-08 16:07:59 +0530

    Coding Challenge #14

--- Coding Challenge/code.js
+++ Coding Challenge/code.js
@@ -347,7 +347,6 @@ document
 poll.displayResults.call({answers: [5, 2, 3]}, 'string')
 poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]}, 'string')
 poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]})
-*/
 
 
 
@@ -360,3 +359,26 @@ poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]})
       header.style.color = 'blue'
    })
 })();
+*/
+
+
+
+// Working with Arrays
+// Coding Challenge #1
+const checkDogs = (dogsJulia, dogsKate) => {
+   const dogsJuliaCorrected = dogsJulia.slice()
+   dogsJuliaCorrected.splice(0, 1)
+   dogsJuliaCorrected.splice(-2)
+   const dogs = dogsJuliaCorrected.concat(dogsKate)
+   dogs.forEach( (dog, i) => {
+      if (dog >= 3) {
+         console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old.`)
+      } else {
+         console.log(`Dog number ${i + 1} is still a puppy 🐶`)
+      }
+   });
+}
+console.log('----TEST DATA 1----')
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
+console.log('----TEST DATA 2----')
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4])
