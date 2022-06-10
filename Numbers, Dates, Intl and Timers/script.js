55d3956c2f61a7b13311ba982699884f249a926e
Anwesh Mishra <anweshm3@gmail.com>
2022-06-10 10:40:51 +0530

    The Remainder Operator

--- Numbers, Dates, Intl and Timers/script.js
+++ Numbers, Dates, Intl and Timers/script.js
@@ -79,3 +79,29 @@ console.log((2.7).toFixed(0))
 console.log((2.7).toFixed(3))
 console.log((2.567).toFixed(2))
 console.log(+(2.567).toFixed(2))
+
+///////////////////////////////////////
+// The Remainder Operator
+console.log(5 % 2)
+console.log(5 / 2) // 5 = 2 * 2 + 1
+
+console.log(8 % 3)
+console.log(8 / 3) // 8 = 2 * 3 + 2
+
+console.log(6 % 2)
+console.log(6 / 2) // 6 = 3 * 2
+
+console.log(7 % 2)
+console.log(7 / 2) // 7 = 3 * 2 + 1
+
+const isEven = n => n % 2 === 0
+console.log(isEven(8))
+console.log(isEven(23))
+console.log(isEven(514))
+
+// labelBalance.addEventListener('click', e => {
+//    [...document.querySelectorAll('.movements__row')].forEach( (row, i) => {
+//      if (i % 2 === 0) row.style.backgroundColor = 'orangered'
+//      if (i % 3 === 0) row.style.backgroundColor = 'blue'
+//    })
+//  })
