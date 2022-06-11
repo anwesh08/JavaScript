5c4ddaf38677b99c72ba701f03d2073da764da4d
Anwesh Mishra <anweshm3@gmail.com>
2022-06-11 11:16:59 +0530

    Button Scrolling

--- Bankist Site/script.js
+++ Bankist Site/script.js
@@ -1,32 +1,44 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to')
+const section1 = document.getElementById('section--1')
 
-const openModal = function () {
+///////////////////////////////////////
+// Modal window
+
+const openModal = e => {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
+const closeModal = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
 for (const btn of btnsOpenModal) {
-   btn.addEventListener('click', openModal)
+  btn.addEventListener('click', openModal);
 }
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
+document.addEventListener('keydown', e => {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
+
+///////////////////////////////////////
+// Button scrolling
+btnScrollTo.addEventListener('click', () => {
+   section1.scrollIntoView({ behavior: "smooth" })
+})
+
+///////////////////////////////////////
+// Page navigation
