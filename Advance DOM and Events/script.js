ee909091370f73905ab740b337c1a09e0699f860
Anwesh Mishra <anweshm3@gmail.com>
2022-06-11 12:09:00 +0530

    Event Propagation in Practice

--- Advance DOM and Events/script.js
+++ Advance DOM and Events/script.js
@@ -112,3 +112,28 @@ setTimeout(() => {
 //    console.log(e)
 //    alert('addEventListener: Great! You are reading the heading :D')
 // }
+
+///////////////////////////////////////
+// Event Propagation in Practice
+const randomInt = (min, max) => 
+   Math.floor(Math.random() * (max- min + 1) + min)
+const randomColor = () => `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`
+console.log(randomColor())
+
+/*
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+   this.style.backgroundColor = randomColor();
+   console.log('LINK', e.target, e.currentTarget);
+   console.log(e.currentTarget === this);
+   // Stop propagation
+   // e.stopPropagation();
+ });
+ document.querySelector('.nav__links').addEventListener('click', function (e) {
+   this.style.backgroundColor = randomColor();
+   console.log('CONTAINER', e.target, e.currentTarget);
+ });
+ document.querySelector('.nav').addEventListener('click', function (e) {
+   this.style.backgroundColor = randomColor();
+   console.log('NAV', e.target, e.currentTarget);
+ });
+ */
