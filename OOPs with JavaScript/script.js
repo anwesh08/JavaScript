8d0acd1b41edc0ec1be31f7e2457ba6af98d8cde
Anwesh Mishra <anweshm3@gmail.com>
2022-06-14 11:46:43 +0530

    Object.create

--- OOPs with JavaScript/script.js
+++ OOPs with JavaScript/script.js
@@ -151,3 +151,27 @@ const account = {
 console.log(account.latest)
 account.latest = 50
 console.log(account.movements)
+
+///////////////////////////////////////
+// Object.create
+const PersonProto = {
+   calcAge() {
+      console.log(2037 - this.birthYear);
+   },
+   init(firstName, birthYear) {
+      this.firstName = firstName
+      this.birthYear = birthYear
+   }
+}
+
+const steven = Object.create(PersonProto)
+console.log(steven)
+steven.name = 'Steven'
+steven.birthYear = 1990
+steven.calcAge()
+
+console.log(steven.__proto__ === PersonProto)
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
