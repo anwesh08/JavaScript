41887dafe91aeb60ae5486a20a277df4f285ea36
Anwesh Mishra <anweshm3@gmail.com>
2022-06-15 11:12:19 +0530

    Coding Challenge #20

--- Coding Challenge/code.js
+++ Coding Challenge/code.js
@@ -478,7 +478,7 @@ car1.accelerate()
 car1.brake()
 car1.accelerate()
 car1.brake()
-*/
+
 
 
 
@@ -510,3 +510,43 @@ ford.accelerate()
 ford.brake()
 ford.speedUS = 50
 console.log(ford)
+*/
+
+// Coding Challenge #3
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/hr`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/hr`);
+};
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`
+  );
+};
+
+const tesla = new EV("Tesla", 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla);
+tesla.brake();
+tesla.accelerate()
