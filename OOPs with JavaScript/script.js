43b31c170d26f922738d884e3dc6a6e3c7ffe49b
Anwesh Mishra <anweshm3@gmail.com>
2022-06-15 17:38:45 +0530

    Another Class Example

--- OOPs with JavaScript/script.js
+++ OOPs with JavaScript/script.js
@@ -294,3 +294,45 @@ const jay = Object.create(StudentProto)
 jay.init("Jay", 2010, 'CSE')
 jay.introduce()
 jay.calcAge()
+
+///////////////////////////////////////
+// Another Class Example
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner
+    this.currency = currency
+    this.pin = pin
+    this.movements = []
+    this.locale = navigator.language
+    console.log(`Thanks for opening an account, ${owner}`)
+  }
+  // Public Interface
+  deposit(val) {
+    this.movements.push(val)
+  }
+  withdraw(val) {
+    this.deposit(-val)
+  }
+  approveLoan(val) {
+    return true
+  }
+  requestLoan(val) {
+    if(this.approveLoan(val)) {
+      this.deposit(val)
+      console.log(`Loan Approved`)
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111, [])
+console.log(acc1)
+
+// acc1.movements.push(250)
+// acc1.movements.push(-50)
+
+acc1.deposit(250)
+acc1.withdraw(40)
+acc1.requestLoan(1000)
+
+console.log(acc1)
+console.log(acc1.pin)
