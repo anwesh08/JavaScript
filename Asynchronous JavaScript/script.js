499ff6161e9d7b4488c990be7d7b617baed9b52c
Anwesh Mishra <anweshm3@gmail.com>
2022-06-23 08:40:19 +0530

    First Commit

--- Asynchronous JavaScript/script.js
+++ Asynchronous JavaScript/script.js
@@ -1,4 +1,7 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+///////////////////////////////////////
+// Our First AJAX Call: XMLHttpRequest
+const request = new XMLHttpRequest() //Old school way of doing AJAX in JS
+request.open('GET', `https://restcountries.eu/rest/v2/name/portugal`)
+request.send()
