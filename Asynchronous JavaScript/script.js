4cc323e6f4a6d0bf6ee218872d675fca6bf814e4
Anwesh Mishra <anweshm3@gmail.com>
2022-06-24 22:55:13 +0530

    Handling Rejected Promises

--- Asynchronous JavaScript/script.js
+++ Asynchronous JavaScript/script.js
@@ -119,4 +119,58 @@ const getCountryData = country => {
     .then(data => renderCountry(data, 'neighbour'))
 };
 getCountryData('germany');
+
+// Handling Rejected Promises
+const renderCountry = (data, className = '') => {
+  const html = `
+        <article class="country ${className}">
+           <img class="country__img" src="${data.flags.svg}" />
+           <div class="country__data">
+              <h3 class="country__name">${data.name}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(
+                +data.population / 1000000
+              ).toFixed(1)} people</p>
+              <p class="country__row"><span>🗣️</span>${
+                data.languages[0].name
+              }</p>
+              <p class="country__row"><span>💰</span>${
+                data.currencies[0].code
+              }</p>
+           </div>
+        </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = 1;
+};
+
+const renderError = msg => {
+   countriesContainer.insertAdjacentText('beforeend', msg)
+   // countriesContainer.style.opacity = 1
+}
+
+const request = fetch('https://restcountries.com/v2/name/portugal');
+console.log(request);
+
+const getCountryData = country => {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(`${err} 💥💥💥`)
+      renderError(`Something went wrong 💥💥💥 ${err.message}. Try Again!`)
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    })
+};
+btn.addEventListener('click', () => {
+  getCountryData('germany');
+});
 */
