e94c079067c6e6e6e1f4d7f0c4f11dc5f6f39809
Anwesh Mishra <anweshm3@gmail.com>
2022-06-26 11:02:31 +0530

    Final Commit

--- Countries/script.js
+++ Countries/script.js
@@ -1,4 +1,49 @@
-'use strict'
+'use strict';
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = (data, className = '') => {
+  const html = `
+        <article class="country ${className}">
+           <img class="country__img" src="${data.flags.svg}" />
+           <div class="country__data">
+              <h3 class="country__name">${data.name}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(
+                +data.population / 1000000
+              ).toFixed(1)} people</p>
+              <p class="country__row"><span>🗣️</span>${
+                data.languages[0].name
+              }</p>
+              <p class="country__row"><span>💰</span>${
+                data.currencies[0].code
+              }</p>
+           </div>
+        </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = country => {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(`${err} 💥💥💥`)
+      renderError(`Something went wrong 💥💥💥 ${err.message}. Try Again!`)
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    })
+};
+btn.addEventListener('click', () => {
+  getCountryData('germany');
+});
