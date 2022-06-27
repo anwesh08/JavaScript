2412eb02ec5889b3a34df63da88c16f93cec02df
Anwesh Mishra <anweshm3@gmail.com>
2022-06-27 22:55:15 +0530

    The Event Loop in Practice

--- Asynchronous JavaScript/script.js
+++ Asynchronous JavaScript/script.js
@@ -203,3 +203,13 @@ btn.addEventListener('click', () => {
   getCountryData('germany');
 });
 */
+
+///////////////////////////////////////
+// The Event Loop in Practice
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
